21ba1f9d16485f65c8d7885bbf5601aebaf37d71
Ben Anderson <benanderson@acidic.co.nz>
2013-12-09 00:07:42 +1300

    Still not working render

--- res/shaders/SimpleVertexShader.vert
+++ res/shaders/SimpleVertexShader.vert
@@ -1,10 +1,10 @@
 #version 330 core
 layout(location = 0) in vec3 vertexPosition_modelspace;
-layout(location = 1) in vec3 vertexUV;
+layout(location = 1) in vec2 vertexUV;
 
 uniform mat4 MVP;
 
-out vec3 UV;
+out vec2 UV;
 
 void main() {
 	gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
--- src/game/main.cpp
+++ src/game/main.cpp
@@ -56,13 +56,14 @@ int main(void) {
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
+    window = glfwCreateWindow(1024, 768, "Simple example", NULL, NULL);
+    if (window == NULL)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
 	if (!GLEE_VERSION_1_2) {
 		cout << "DO SOMETHING!" << endl;
@@ -94,24 +95,26 @@ int main(void) {
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 
-    double newverts[vertices.size()*3];
-    for(int i = 0; i < vertices.size()*3; ) {
+    struct simplevert {
+        float x, y, z;
+    };
 
-        newverts[i+0] = vertices[i/3].x();
-        newverts[i+1] = vertices[i/3].y();
-        newverts[i+2] = vertices[i/3].z();
+    simplevert newverts[vertices.size()];
+    for(int i = 0; i < vertices.size(); i++) {
 
-        if(nearzero(newverts[i])) newverts[i] = 0;
-        if(nearzero(newverts[i+1])) newverts[i+1] = 0;
-        if(nearzero(newverts[i+2])) newverts[i+2] = 0;
+        newverts[i].x = vertices[i].x();
+        newverts[i].y = vertices[i].y();
+        newverts[i].z = vertices[i].z();
 
-        i+=3;
+        // if(nearzero(newverts[i])) newverts[i] = 0;
+        // if(nearzero(newverts[i+1])) newverts[i+1] = 0;
+        // if(nearzero(newverts[i+2])) newverts[i+2] = 0;
     }
 
     for(int i = 0; i < vertices.size(); i++)
-        cout << i << ":" << newverts[i] << endl;
+        cout << i << ":" << newverts[i].x << " " << newverts[i].y << " " << newverts[i].z << endl;
 
-    glBufferData(GL_ARRAY_BUFFER, 36 * 3 * sizeof(double), &newverts[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(simplevert), &newverts[0], GL_STATIC_DRAW);
 
     GLuint colorBuffer;
     glGenBuffers(1, &colorBuffer);
@@ -127,19 +130,20 @@ int main(void) {
     mat4d Projection =  createPerspectiveFOV(45, 4.0f / 3.0f, 0.1f, 10000.0f);
     mat4d View = createLookAt(pos, look, up);
     mat4d Model = mat4d(3.0f);
+    mat4<float> MVP = Projection * View * Model;
 
     cout << "Pos: " << pos << endl << "Look: " << look << endl << "Up: " << up << endl;
     cout << "Projection: " << endl << Projection << endl;
     cout << "View: " << endl << View << endl;
     cout << "Model: " << endl << Projection << endl;
+    cout << "MVP:" << endl << MVP << endl;
 
-    double longitude = 0;
-    double elevation = 20;
-    double zoom = 20;
+        // cout << "View: " << endl << View << endl;
+    
 
     double lastFPSTime = glfwGetTime();
     int fps = 0;
-    
+
     // Vector3 vec();
     
     do {
@@ -147,53 +151,17 @@ int main(void) {
 
         double now = glfwGetTime();
         if (now - lastFPSTime > 1) {
-            // printf("FPS: %d\n", fps);
+            printf("FPS: %d\n", fps);
             fps = 0;
             lastFPSTime = glfwGetTime();
         }
         fps++;
 
-        if (glfwGetKey(window, GLFW_KEY_A))
-            longitude -= 0.05f;
-        if (glfwGetKey(window, GLFW_KEY_D))
-            longitude += 0.05f;
-
-        if (glfwGetKey(window, GLFW_KEY_W))
-            elevation += 0.1;
-        if (glfwGetKey(window, GLFW_KEY_S))
-            elevation -= 0.1;
-
-        if (glfwGetKey(window, GLFW_KEY_E))
-            zoom += 1;
-        if (glfwGetKey(window, GLFW_KEY_Q))
-            zoom -= 1;
-
-        if (zoom < 1)
-            zoom = 1;
-        else if (zoom > 30)
-            zoom = 30;
-
-        if (longitude > M_PI * 2)
-            longitude -= (M_PI * 2);
-
-        if (elevation > 40)
-            elevation = 40;
-        if (elevation < 0)
-            elevation = 0;
-
-        pos = vec3d(cos(longitude) * zoom, elevation, sin(longitude) * zoom);
-        // cout << "Pos: " << pos << endl;
-        View = createLookAt(pos, look, up);
-        // cout << "View: " << endl << View << endl;
-        mat4d MVP = Projection * View * Model;
-        // cout << "MVP: " << endl << MVP << endl;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-        float mvpFloat = (float)MVP(0, 0);
-        // cout << "mvpFloat: " << mvpFloat << endl;
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, (float*)(&MVP(0, 0)));
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP(0, 0));
         glUseProgram(programID);
 
         glEnable(GL_DEPTH_TEST);
@@ -204,7 +172,7 @@ int main(void) {
         glVertexAttribPointer(
             0,
             3,
-            GL_DOUBLE,
+            GL_FLOAT,
             GL_FALSE,
             0,
             (void*)0
@@ -214,15 +182,15 @@ int main(void) {
         glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
         glVertexAttribPointer(
             1,
-            3,
-            GL_DOUBLE,
+            2,
+            GL_FLOAT,
             GL_FALSE,
             0,
             (void*)0
         );
 
         glDrawArrays(GL_TRIANGLES, 0, 36);
-        // glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
     } while(!glfwWindowShouldClose(window));
